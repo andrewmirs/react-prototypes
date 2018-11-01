68da6d7e007f202f457ec428d1914eba079c0a4f
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-01 15:29:28 -0700

    fs1 done

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div className="container">
+            <h1 className="text-center">Stopwatch</h1>
+        </div>
+    )
+}
+
 
 
