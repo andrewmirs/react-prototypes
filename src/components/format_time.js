54ffacd7135d99e524065bfaf1a201efcb4280b8
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-01 17:12:06 -0700

    fs5 completed

--- src/components/format_time.js
+++ src/components/format_time.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { relative } from 'path';
 
 class FormatTime extends Component {
     constructor(props){
@@ -8,6 +9,13 @@ class FormatTime extends Component {
             min: 0,
             sec: 0,
             ms: 0
+        };
+        this.timerStyle = {
+            display: 'inline-block',
+            position: 'relative',
+            left: '50%',
+            transform: 'translate(-50%)',
+            width: '295px'
         };
     }
     componentWillReceiveProps(nextProps){
@@ -25,7 +33,7 @@ class FormatTime extends Component {
     render(){
         const { hour, min, sec, ms } = this.state;
         return(
-            <div>{ hour }:{ min }:{ sec }.{ ms }</div>
+            <div style={ this.timerStyle }>{ hour }:{ min }:{ sec }.{ ms }</div>
         )
     }
 }
