e1b2a0fba2bd37d34b9fcc5c78910454910a4a35
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-02 22:37:24 -0700

    fs2 part way done: created contact card/list and added full name to DOM

---
+++ src/data/contacts.js
@@ -0,0 +1,26 @@
+export default [ 
+    {
+        firstName: 'Andre',
+        lastName: 'Hendrix',
+        phone: '949-336-4695',
+        email: 'andre5stacks@hotmail.com'
+    },
+    {
+        firstName: 'Linda',
+        lastName: 'Wylie',
+        phone: '857-443-2148',
+        email: 'WylieCoyote@osu.org'
+    },
+    {
+        firstName: 'Davoud',
+        lastName: 'Mirshafiee',
+        phone: '212-865-9959',
+        email: 'Davoudmirs@gmail.com'
+    },
+    {
+        firstName: 'Mateo',
+        lastName: 'Angeles',
+        phone: '714-633-7296',
+        email: 'yogaonthemat@love.com'
+    }
+];
