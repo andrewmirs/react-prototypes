40e9148a1274756ef16fc3cf89e998adfdb5f899
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-09 10:05:47 -0800

    completed fs4

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Welcome from './welcome';
 import Nav from './nav';
+import OurMacarons from './our_macarons';
 
 const App = () => {
     return(
         <div className="container">
             <Nav />
-            <Route path="/" component={Welcome} />
+            <Route exact path="/" component={Welcome} />
+            <Route path="/our-macarons" component={OurMacarons} />
         </div>
     )
 }
---
+++ src/components/our_macarons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OurMacarons = () => {
+    return(
+        <div>
+            <h1>Scrumptious, tasty, delicious, moist macarons</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit a fugit accusantium reiciendis iusto voluptatibus et error ipsam excepturi quidem vitae fugiat voluptates eos enim repellat, maxime at numquam nesciunt!</p>
+        </div>
+    );
+}
+
+export default OurMacarons;
