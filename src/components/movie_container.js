356577268c7a6973224d640cfe1e87eb03c02289
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-12 10:35:45 -0800

    fs2 completed

--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Movie from './movies';
 
 class MovieContainer extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            movies: []
+        }
+    }
+    componentWillMount = () => {
+        const itunesAPI = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
+
+        axios.get(itunesAPI).then((resp) => {
+            this.setState({
+                movies: resp.data.feed.entry
+            })
+        });
+    }
     render(){
+        console.log(this.state);
         return(
             <div>
                 <h2>Movie Container</h2>
