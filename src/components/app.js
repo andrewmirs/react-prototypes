1adad8883afefbbe59adebd56df86deadce439ea
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-16 19:03:07 -0800

    added initial files/folders for redux clock app

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,9 @@
+import React from 'react';
 
-
+export default () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">This is a test</h1>
+        </div>
+    );
+}
