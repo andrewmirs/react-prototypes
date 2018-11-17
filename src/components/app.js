f315d56760af0eab61a7e1fb936f7f0bc8b484ae
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-17 09:18:38 -0800

    added static time to my redux clock,which is now connected to state'

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Clock from './clock';
 
 export default () => {
     return (
         <div className="container">
             <h1 className="text-center">This is a test</h1>
+            <Clock />
         </div>
     );
 }
--- src/reducers/clock_reducer.js
+++ src/reducers/clock_reducer.js
@@ -1,10 +1,11 @@
 
 const DEFAULT_STATE = {
-    time: null
+    time: new Date().toLocaleTimeString()
 };
 
 export default function( state = DEFAULT_STATE, action ) {
     switch(action.type){
-        
+        default:
+            return state;
     }
 }
